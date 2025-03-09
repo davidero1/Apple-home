25826779496cae2d4bec69cbe4bf4300e10b8cab
David <daviderophillips@gmail.com>
2025-03-09 21:46:10 +0100

    adding all files

---
+++ script.js
@@ -0,0 +1,9 @@
+"use-strict";
+
+document.querySelector(".barsicon").addEventListener("click", function() {
+    const navBox = document.querySelector(".navbox");
+    const icon = document.querySelector(".fa-bars");
+    navBox.classList.toggle("show");
+    icon.classList.toggle("fa-xmark");
+});
+
